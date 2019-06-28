11e6e25312f27017104e2df2ab14cc9979e7d48c
rongfengliang <1141591465@qq.com>
2019-06-28 16:24:02 +0800

    add json demo

--- modmain.v
+++ modmain.v
@@ -3,7 +3,22 @@ module main
 
 import fetchbaidu
 
-fn main() {
+import json
 
+struct User {
+    name string
+    age int
+}
+fn main() {
+    s := '{"age": 10, "name": "dalongdemo"}'
+    u := json.decode(User, s) or {
+		exit(1) 
+		return
+	}
+    printuserinfo(u)
     fetchbaidu.fetchindexpage()
 }
+
+fn printuserinfo(u User) {
+    println(u.name)
+} 
